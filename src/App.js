c64af9f97a38792bf11b0cec2f9b9a2b66350097
Cesar <cesar.kina@gmail.com>
2019-08-18 17:41:12 -0500

    Cleans up unnecessary files and configurations after create-react-app execution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+        <h1>Cesar Kina</h1>
+        <a href="https://www.linkedin.com/in/ckina/">
+            <img src="https://img.icons8.com/color/96/000000/linkedin-circled.png" />
         </a>
-      </header>
+        <a href="https://github.com/ckinan">
+            <img src="https://img.icons8.com/color/96/000000/github.png" />
+        </a>
     </div>
   );
 }
