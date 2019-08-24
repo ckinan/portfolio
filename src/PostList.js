85dbf6381bfe6f70ed3b056944361876f239ffd5
unknown <cesar.kina@gmail.com>
2019-08-24 10:20:47 -0500

    Routing react views

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import { Link } from "react-router-dom";
 
 class PostList extends React.Component {
     render() {
@@ -8,7 +8,9 @@ class PostList extends React.Component {
                 <h1>Articles</h1>
                 <div className="post-list-item">
                     <span className="date">22-Aug-2019</span>
-                    <span className="title"><a href="#">ReactJS</a></span>
+                    <span className="title">
+                        <Link to="/react-js/">ReactJS</Link>
+                    </span>
                 </div>
                 <div className="post-list-item">
                     <span className="date">20-Aug-2019</span>
