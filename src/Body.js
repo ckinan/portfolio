3ddfeed8aa673452ea23c32c2db5f30dbe720644
Cesar <cesar.kina@gmail.com>
2019-08-27 21:56:19 -0500

    Get rid of markdown

--- src/Body.js
+++ src/Body.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Home from './Home';
-import MarkDownTest from './MarkDownTest';
 import { Switch, Route } from 'react-router-dom';
 
 function Body() {
   return (
       <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/react-js' component={MarkDownTest}/>
       </Switch>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PostList from './PostList';
 
 class Home extends React.Component {
     render() {
@@ -8,8 +7,6 @@ class Home extends React.Component {
                 <h1>Hi, Cesar here</h1>
                 <p>Software Developer</p>
                 <p>Some years of experience coding</p>
-                <p>Hopefully, today, I will learn something new</p>
-                <PostList />
             </div>
           );
     }
