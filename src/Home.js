9ddc49a35d2f893aabb50bc8303cf850292851d2
Cesar <cesar.kina@gmail.com>
2019-08-27 22:27:47 -0500

    Update summary

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,12 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hi, Cesar here</h1>
-                <p>Software Developer</p>
-                <p>Some years of experience coding</p>
+                <h1>Cesar K.</h1>
+                <p>Software Engineer</p>
+                <h1>About me</h1>
+                <p>Around 10 years of experience in software development projects.</p>
+                <p>Can adapt to different approaches and work-styles with ease.</p>
+                <p>Analytical skills, self-learner and aware of the software quality.</p>
             </div>
           );
     }
