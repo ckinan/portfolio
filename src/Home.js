926ad2341f73f74e92298b7003cf038ca41ebd6c
Cesar <cesar.kina@gmail.com>
2019-08-28 21:16:48 -0500

    Update home text

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,6 @@ class Home extends React.Component {
                 <p>Software Engineer</p>
                 <h1>About me</h1>
                 <p>Over 10 years of experience in software development projects.</p>
-                <p>Can adapt to different approaches and work-styles with ease.</p>
                 <p>Analytical skills, self-learner and aware of the software quality.</p>
             </div>
           );
