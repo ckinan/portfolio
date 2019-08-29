2bf3604ba475fd1f8265e41f43ea34a7010b1157
Cesar <cesar.kina@gmail.com>
2019-08-28 21:42:11 -0500

    Update dark theme

--- src/Header.js
+++ src/Header.js
@@ -8,17 +8,17 @@ class Header extends React.Component {
                 <ul className="nav">
                     <li>
                         <Link to="/">
-                            <img src="https://img.icons8.com/bubbles/30/000000/home-page.png" alt="Home"/>
+                            <img src="https://img.icons8.com/bubbles/96/000000/home-page.png" alt="Home"/>
                         </Link>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/ckina/" target="_blank" rel="noopener noreferrer">
-                            <img src="https://img.icons8.com/color/30/000000/linkedin-circled.png" alt="Linkedin"/>
+                            <img src="https://img.icons8.com/color/96/000000/linkedin-circled.png" alt="Linkedin"/>
                         </a>
                     </li>
                     <li>
                         <a href="https://github.com/ckinan" target="_blank" rel="noopener noreferrer">
-                            <img src="https://img.icons8.com/color/30/000000/github.png" alt="GitHub"/>
+                            <img src="https://img.icons8.com/color/96/000000/github.png" alt="GitHub"/>
                         </a>
                     </li>
                     <li>
