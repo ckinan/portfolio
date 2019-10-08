9feccd31c338e59ef0466c3a84af6be6653d804b
Cesar <cesar.kina@gmail.com>
2019-10-08 18:02:44 -0500

    Gatsby blog starter

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,20 @@
+---
+title: Hello World
+date: "2019-10-08T18:01:00.000Z"
+description: "Hello World"
+---
+
+# h1 Heading
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+## Tables
+
+| a | b |
+| --- | --- |
+| 1 | x |
+| 2 | y |
+| 3 | z |
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,8 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h1>Cesar K.</h1>
-                <p>Software Engineer</p>
                 <h1>About me</h1>
+                <p>Software Engineer</p>
                 <p>Over 10 years of experience in software development projects.</p>
                 <p>Analytical skills, self-learner and aware of the software quality.</p>
             </div>
