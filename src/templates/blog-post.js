2f84af801bdaf58cc8b6f1e871a8d44bdab9d173
Cesar <cesar.kina@gmail.com>
2019-10-08 22:16:43 -0500

    styling

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -32,25 +32,6 @@ class BlogPostTemplate extends React.Component {
             <Bio />
           </footer>
         </article>
-
-        <nav>
-          <ul>
-            <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav>
       </Layout>
     )
   }
