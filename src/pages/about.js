02e46437b22de3a7f073d08dcb1a48d81cb22b53
ckinan <cesar.kina@gmail.com>
2020-04-25 08:30:38 -0500

    Introducing gatsbyjs

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Link to="/">Home</Link>
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/about/">About</Link>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+  </div>
+)
