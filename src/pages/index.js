bf390fa0c20aad0c48dc130b1634f517b540e9a9
ckinan <cesar.kina@gmail.com>
2020-04-26 13:39:44 -0500

    Minor styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,17 +38,22 @@ export default () => {
       <div className="container mx-auto p-4 max-w-screen-md">
         <div>
           {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div key={node.id} className="hover:bg-gray-200">
-              <span className="grid grid-cols-1 lg:grid-cols-6 border-b py-2">
+            <div
+              key={node.id}
+              className="hover:bg-gray-100 border-b overflow-hidden"
+            >
+              <span>
                 <Link
-                  className="col-span-6 lg:col-span-4"
+                  className="grid grid-cols-1 lg:grid-cols-6 p-3"
                   to={node.fields.slug}
                 >
-                  {node.frontmatter.title}
+                  <span className="col-span-6 lg:col-span-4">
+                    {node.frontmatter.title}
+                  </span>
+                  <span className="col-span-6 lg:col-span-2 text-left text-xs text-gray-600 lg:text-right">
+                    {node.frontmatter.date}
+                  </span>
                 </Link>
-                <span className="col-span-6 lg:col-span-2 text-left text-xs text-gray-600 lg:text-right">
-                  {node.frontmatter.date}
-                </span>
               </span>
             </div>
           ))}
