6fcae1cf8060bcb5b80bd039c0c824b7b9429ea2
ckinan <cesar.kina@gmail.com>
2020-07-24 23:46:34 -0500

    adding utteranc for comments

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql } from "gatsby"
 import { BlogLayout } from "../components/layout"
 import { Helmet } from "react-helmet"
@@ -11,6 +11,19 @@ const components = {
 }
 
 export default ({ data }) => {
+  useEffect(() => {
+    let script = document.createElement("script")
+    let anchor = document.getElementById("comments")
+    script.setAttribute("src", "https://utteranc.es/client.js")
+    script.setAttribute("repo", "ckinan/ckinan.com")
+    script.setAttribute("issue-term", "pathname")
+    script.setAttribute("label", "comments")
+    script.setAttribute("theme", "github-light")
+    script.setAttribute("crossorigin", "anonymous")
+    script.setAttribute("async", true)
+    anchor.appendChild(script)
+  }, [])
+
   const post = data.mdx
   return (
     <BlogLayout>
@@ -28,6 +41,10 @@ export default ({ data }) => {
           <MDXProvider components={components}>
             <MDXRenderer>{post.body}</MDXRenderer>
           </MDXProvider>
+        </div>
+        <div className="border-t mt-16">
+          <div className="mt-4 text-2xl">Comments</div>
+          <div id="comments"></div>
         </div>
       </div>
     </BlogLayout>
