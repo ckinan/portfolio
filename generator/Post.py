981b068df3c9e674aea9121198c571d70cac5760
Cesar <cesar.kina@gmail.com>
2021-05-23 08:15:01 -0500

    Revert "include content in rss feed"
    This reverts commit bcea81d5c73ed0e9c485b8b81ecde30aed989595.

--- generator/Post.py
+++ generator/Post.py
@@ -3,13 +3,12 @@ import json
 
 class Post:
 
-    def __init__(self, title, date, slug, directory, content, html):
+    def __init__(self, title, date, slug, directory, content):
         self.title = title
         self.date = date
         self.slug = slug
         self.directory = directory
         self.content = content
-        self.html = html
 
     def __repr__(self):
         return json.dumps(self.__dict__)
