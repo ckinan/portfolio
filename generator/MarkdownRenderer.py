351dedb0296b876accecaabed3e6371ef500b2b2
Cesar <cesar.kina@gmail.com>
2021-06-19 12:59:21 -0500

    add pygments

--- generator/MarkdownRenderer.py
+++ generator/MarkdownRenderer.py
@@ -1,5 +1,9 @@
 import mistune
 import re
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+import traceback
 
 
 class MarkdownRenderer:
@@ -16,3 +20,23 @@ class HighlightRenderer(mistune.Renderer):
     def header(self, text, level, raw=None):
         header_id = re.sub('[^0-9a-zA-Z]+', '-', text).lower()
         return f'<h{level} id="{header_id}"><a href="#{header_id}" style="margin-right: 0.25rem;">#</a>{text}</h{level}>\n'
+
+    def block_code(self, code, lang=None):
+        if not lang:
+            lang = 'text'
+
+        try:
+            code = highlight(
+                code,
+                get_lexer_by_name(lang, stripall=True),
+                HtmlFormatter(
+                    noclasses=True,
+                    # linenos='table',
+                )
+            )
+            return code
+        except:
+            print(lang)
+            traceback.print_exc()
+            return f'<pre class="{lang}"><code>{mistune.escape(code)}</code></pre>\n'
+
